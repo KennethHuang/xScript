e453b7ed7496d9d33bd29a09e54df1d845be59f5
yourenzhuce <yourenzhuce@gmail.com>
2015-06-08 14:05:20 +0800

    Add xScript.database part.

--- bin/xScript.bat
+++ bin/xScript.bat
@@ -1,1 +1,49 @@
-java -Xmx512m -Xms512m -jar jboss-modules.jar -mp "..\modules" kenh.xscript
+@echo off
+set inPath=%1
+if defined inPath (
+  goto op1
+) else (
+  goto tip
+)
+
+goto end
+
+:tip
+
+echo Please use -f:{file} or -file:{file} to specify the xScript doc.
+goto end
+
+:op1
+
+set fstr=%inPath:~0,3%
+
+if "%fstr%"=="-f:" (
+  set fstr_after=%inPath:~3%
+
+  if defined fstr_after ( 
+    goto op2 
+  ) else (
+    goto tip
+  )
+) else (
+  set fistr=%inPath:~0,6%
+  
+  if "%fistr%"=="-file:" (
+    set fistr_after=%inPath:~6%
+	
+	if defined fistr_after ( 
+	  goto op2 
+	) else (
+	  goto tip
+	)
+  )
+)
+
+goto end
+
+:op2
+
+java -Xmx512m -Xms512m -jar jboss-modules.jar -mp "..\modules" kenh.xscript %1
+goto end
+
+:end
